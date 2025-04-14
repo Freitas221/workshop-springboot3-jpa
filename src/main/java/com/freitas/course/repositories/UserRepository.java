6da165ebd8e5162dbff487a2b1734a9c2fc537f2
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-04-14 19:40:41 -0300

    Service layer, component registration

--- src/main/java/com/freitas/course/repositories/UserRepository.java
+++ src/main/java/com/freitas/course/repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.freitas.course.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.freitas.course.entities.User;
 
+@Repository //Optional, since JpaRepository is already registered
+            //as a Spring component.
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
