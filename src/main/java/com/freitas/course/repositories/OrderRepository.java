57f5f4319fb64c6077bee4e06fb621e7921627d6
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-04-21 14:25:37 -0300

    Order, Instant, ISO 8601 - PART 2

--- src/main/java/com/freitas/course/repositories/OrderRepository.java
+++ src/main/java/com/freitas/course/repositories/OrderRepository.java
@@ -3,11 +3,10 @@ package com.freitas.course.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.freitas.course.entities.User;
+import com.freitas.course.entities.Order;
 
 @Repository //Optional, since JpaRepository is already registered
             //as a Spring component.
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface OrderRepository extends JpaRepository<Order, Long>{
 
 }
-
